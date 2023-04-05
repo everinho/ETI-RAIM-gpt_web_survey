5e9534eae03d8edd97c60b8fbb0e556d53fed595
Antoni Górecki <117845737+everinho@users.noreply.github.com>
2023-04-05 19:48:27 +0000

    Added end screen, applied lots of changes

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from database import add_answers_to_db
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
   return render_template('home.html')
+
+@app.route("/end")
+def final_page():   
+    return render_template('end.html')
+
+@app.route("/form/end", methods=['get'])
+def submit_page():   
+  data = request.form
+
+  add_answers_to_db(data)
+  return render_template('end.html', data=data)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,6 +1,7 @@
+import os
 from sqlalchemy import create_engine, text
 
-db_connection_string = "mysql+pymysql://9ylf6aw63keny1r5bggz:pscale_pw_ImaonUBaSUlGQEkXGj4MsD4lfcyO6FAhkEYSmEEAgDk@aws.connect.psdb.cloud/gptdatabase?charset=utf8mb4"
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(
   db_connection_string, 
@@ -9,9 +10,10 @@ engine = create_engine(
       "ssl_ca": "/etc/ssl/cert.pem"
     }
   }
-
 )
 
-with engine.connect() as conn:
-  result = conn.execute(text("select * from users"))
-  print(result.all())
+
+def add_answers_to_db(data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO application (first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth, eleventh, twelfth, thirteenth, fourteenth, fifteenth, sixteenth, seventeenth, eighteenth, nineteenth, twentieth, twenty-first, twenty-second, twenty-third, twenty-fourth) VALUES (first, :second, :third, :fourth, :fifth, :sixth, :seventh, :eighth, :ninth, :tenth, :eleventh, :twelfth, :thirteenth, :fourteenth, :fifteenth, :sixteenth, seventeenth, :eighteenth, :nineteenth, :twentieth, :twenty-first, :twenty-second, :twenty-third, :twenty-fourth)") 
+    conn.execute(query, {'first': data['first'],'second': data['second'], 'third' : data['third'], 'fourth' : data['fourth'], 'fifth' : data['fifth'], 'sixth': data['sixth'] , 'seventh': data['seventh'], 'eighth': data['eighth'], 'ninth': data['ninth'],'tenth' : data['tenth'], 'eleventh' : data['eleventh'], 'twelfth' : data['twelfth'], 'thirteenth' : data['thirteenth'], 'fourteenth' : data['fourteenth'], 'fifteenth' : data['fifteenth'], 'sixteenth': data['sixteenth'], 'seventeenth' : data['seventeenth'], 'eighteenth' : data['eighteenth'], 'nineteenth' : data['nineteenth'], 'twentieth' : data['twentieth'], 'twenty-first': data['twenty-first'],'twenty-second': data['twenty-second'], 'twenty-third' : data['twenty-third'], 'twenty-fourth' : data['twenty-fourth']})

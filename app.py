520bfae0ec95f5f33482653285becd67eb9c7b75
Antoni Górecki <117845737+everinho@users.noreply.github.com>
2023-04-06 21:43:11 +0000

    testing other db, some changes

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request
-from database import add_answers_to_db
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -7,16 +6,21 @@ app = Flask(__name__)
 def hello_world():
   return render_template('home.html')
 
-@app.route("/end")
-def final_page():   
-    return render_template('end.html')
+# @app.route("/end")
+# def final_page():   
+#     return render_template('end.html')
+
+# @app.route("/form/end", methods=['get'])
+# def submit_page():   
+#   data = request.form
+
+#   add_answers_to_db(data)
+#   return render_template('end.html', data=data)
 
 @app.route("/form/end", methods=['get'])
-def submit_page():   
-  data = request.form
-
-  add_answers_to_db(data)
-  return render_template('end.html', data=data)
+def poll_submit():
+  data = request.args
+  return render_template('end.html', answers = data)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -2,18 +2,30 @@ import os
 from sqlalchemy import create_engine, text
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
+db_connect = os.environ['db_connect']
 
-engine = create_engine(
-  db_connection_string, 
-  connect_args={
-    "ssl": {
-      "ssl_ca": "/etc/ssl/cert.pem"
-    }
+engine = create_engine(db_connect, connect_args = {
+  "ssl": {
+    "ssl_ca": "/etc/ssl/cert.pem"
   }
-)
+})
 
-
-def add_answers_to_db(data):
+def full_db(answers):
   with engine.connect() as conn:
-    query = text("INSERT INTO application (first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth, eleventh, twelfth, thirteenth, fourteenth, fifteenth, sixteenth, seventeenth, eighteenth, nineteenth, twentieth, twenty-first, twenty-second, twenty-third, twenty-fourth) VALUES (first, :second, :third, :fourth, :fifth, :sixth, :seventh, :eighth, :ninth, :tenth, :eleventh, :twelfth, :thirteenth, :fourteenth, :fifteenth, :sixteenth, seventeenth, :eighteenth, :nineteenth, :twentieth, :twenty-first, :twenty-second, :twenty-third, :twenty-fourth)") 
-    conn.execute(query, {'first': data['first'],'second': data['second'], 'third' : data['third'], 'fourth' : data['fourth'], 'fifth' : data['fifth'], 'sixth': data['sixth'] , 'seventh': data['seventh'], 'eighth': data['eighth'], 'ninth': data['ninth'],'tenth' : data['tenth'], 'eleventh' : data['eleventh'], 'twelfth' : data['twelfth'], 'thirteenth' : data['thirteenth'], 'fourteenth' : data['fourteenth'], 'fifteenth' : data['fifteenth'], 'sixteenth': data['sixteenth'], 'seventeenth' : data['seventeenth'], 'eighteenth' : data['eighteenth'], 'nineteenth' : data['nineteenth'], 'twentieth' : data['twentieth'], 'twenty-first': data['twenty-first'],'twenty-second': data['twenty-second'], 'twenty-third' : data['twenty-third'], 'twenty-fourth' : data['twenty-fourth']})
+    query = text("INSERT INTO answers (wiek, plec, wyksztalcenie, miejscezamieszkania, wiedza, praca, zadania, efektywnosc, rozrywka, ocena, watpliwosci, klamstwo, leczenie, choroby, uleczalnosc, wyborchorob, terapia, ibm, krzeslo, wypowiedz, udar, zastosowanie, korzysci, aspekty ) VALUES (:wiek, :plec, :wyksztalcenie, :miejscezamieszkania, :wiedza, :praca, :zadania, :efektywnosc, :rozrywka, :ocena, :watpliwosci, :klamstwo, :leczenie, :choroby, :uleczalnosc, :wyborchorob, :terapia, :ibm, :krzeslo, :wypowiedz, :udar, :zastosowanie, :korzysci, :aspekty)")
+  conn.execute(query, {})
+
+# engine = create_engine(
+#   db_connection_string, 
+#   connect_args={
+#     "ssl": {
+#       "ssl_ca": "/etc/ssl/cert.pem"
+#     }
+#   }
+# )
+
+# def add_answers_to_db(data):
+#   with engine.connect() as conn:
+#     query = text("INSERT INTO application (first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth, eleventh, twelfth, thirteenth, fourteenth, fifteenth, sixteenth, seventeenth, eighteenth, nineteenth, twentieth, twenty-first, twenty-second, twenty-third, twenty-fourth) VALUES (first, :second, :third, :fourth, :fifth, :sixth, :seventh, :eighth, :ninth, :tenth, :eleventh, :twelfth, :thirteenth, :fourteenth, :fifteenth, :sixteenth, seventeenth, :eighteenth, :nineteenth, :twentieth, :twenty-first, :twenty-second, :twenty-third, :twenty-fourth)") 
+#     conn.execute(query, {'first': data['first'],'second': data['second'], 'third' : data['third'], 'fourth' : data['fourth'], 'fifth' : data['fifth'], 'sixth': data['sixth'] , 'seventh': data['seventh'], 'eighth': data['eighth'], 'ninth': data['ninth'],'tenth' : data['tenth'], 'eleventh' : data['eleventh'], 'twelfth' : data['twelfth'], 'thirteenth' : data['thirteenth'], 'fourteenth' : data['fourteenth'], 'fifteenth' : data['fifteenth'], 'sixteenth': data['sixteenth'], 'seventeenth' : data['seventeenth'], 'eighteenth' : data['eighteenth'], 'nineteenth' : data['nineteenth'], 'twentieth' : data['twentieth'], 'twenty-first': data['twenty-first'],'twenty-second': data['twenty-second'], 'twenty-third' : data['twenty-third'], 'twenty-fourth' : data['twenty-fourth']})
+

0d408050b7d73a55a74a64687b4157cb3cbd3b2b
Antoni Górecki <117845737+everinho@users.noreply.github.com>
2023-04-07 20:23:28 +0000

    added 'required' to inputs, applied some changes

--- app.py
+++ app.py
@@ -7,23 +7,12 @@ app = Flask(__name__)
 def hello_world():
   return render_template('home.html')
 
-# @app.route("/end")
-# def final_page():   
-#     return render_template('end.html')
-
 @app.route("/form/end", methods=['get'])
 def submit_page():   
   data = request.args
 
   full_db(data)
   return render_template('end.html', data=data)
-
-# @app.route("/form/end", methods=['get'])
-# def poll_submit():
-#   data = request.args
-
-#   full_db(data)
-#   return render_template('end.html', answers = data)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 import os
 from sqlalchemy import create_engine, text
 
-# db_connection_string = os.environ['DB_CONNECTION_STRING']
 my_secret = os.environ['db_connect']
 
 engine = create_engine(my_secret, connect_args = {
@@ -14,18 +13,3 @@ def full_db(answers):
   with engine.connect() as conn:
     query = text("INSERT INTO answers (wiek, plec, wyksztalcenie, miescezamieszkania, wiedza, praca, zadania, efektywnosc, rozrywka, ocena, watpliwosci, klamstwo, leczenie, choroby, uleczalnosc, wyborchorob, terapia, ibm, krzeslo, wypowiedz, udar, zastosowanie, korzysci, aspekty ) VALUES (:wiek, :plec, :wyksztalcenie, :miescezamieszkania, :wiedza, :praca, :zadania, :efektywnosc, :rozrywka, :ocena, :watpliwosci, :klamstwo, :leczenie, :choroby, :uleczalnosc, :wyborchorob, :terapia, :ibm, :krzeslo, :wypowiedz, :udar, :zastosowanie, :korzysci, :aspekty)")
     conn.execute(query, {'wiek': answers['wiek'], 'plec': answers['plec'],'wyksztalcenie': answers['wyksztalcenie'],'miescezamieszkania': answers['miescezamieszkania'],'wiedza': answers['wiedza'],'praca': answers['praca'],'zadania': answers['zadania'],'efektywnosc': answers['efektywnosc'],'rozrywka': answers['rozrywka'],'ocena': answers['ocena'],'watpliwosci': answers['watpliwosci'],'klamstwo': answers['klamstwo'],'leczenie': answers['leczenie'], 'choroby': answers['choroby'], 'uleczalnosc': answers['uleczalnosc'], 'wyborchorob': answers['wyborchorob'], 'terapia': answers['terapia'], 'ibm': answers['ibm'], 'krzeslo': answers['krzeslo'], 'wypowiedz': answers['wypowiedz'], 'udar': answers['udar'], 'zastosowanie': answers['zastosowanie'], 'korzysci': answers['korzysci'], 'aspekty': answers['aspekty']})
-
-# engine = create_engine(
-#   db_connection_string, 
-#   connect_args={
-#     "ssl": {
-#       "ssl_ca": "/etc/ssl/cert.pem"
-#     }
-#   }
-# )
-
-# def add_answers_to_db(data):
-#   with engine.connect() as conn:
-#     query = text("INSERT INTO application (first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth, eleventh, twelfth, thirteenth, fourteenth, fifteenth, sixteenth, seventeenth, eighteenth, nineteenth, twentieth, twenty-first, twenty-second, twenty-third, twenty-fourth) VALUES (first, :second, :third, :fourth, :fifth, :sixth, :seventh, :eighth, :ninth, :tenth, :eleventh, :twelfth, :thirteenth, :fourteenth, :fifteenth, :sixteenth, seventeenth, :eighteenth, :nineteenth, :twentieth, :twenty-first, :twenty-second, :twenty-third, :twenty-fourth)") 
-#     conn.execute(query, {'first': data['first'],'second': data['second'], 'third' : data['third'], 'fourth' : data['fourth'], 'fifth' : data['fifth'], 'sixth': data['sixth'] , 'seventh': data['seventh'], 'eighth': data['eighth'], 'ninth': data['ninth'],'tenth' : data['tenth'], 'eleventh' : data['eleventh'], 'twelfth' : data['twelfth'], 'thirteenth' : data['thirteenth'], 'fourteenth' : data['fourteenth'], 'fifteenth' : data['fifteenth'], 'sixteenth': data['sixteenth'], 'seventeenth' : data['seventeenth'], 'eighteenth' : data['eighteenth'], 'nineteenth' : data['nineteenth'], 'twentieth' : data['twentieth'], 'twenty-first': data['twenty-first'],'twenty-second': data['twenty-second'], 'twenty-third' : data['twenty-third'], 'twenty-fourth' : data['twenty-fourth']})
-
